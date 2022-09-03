9263ca3bc38f971b4265d2ff56fb6a05a5e6855f
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-09-03 17:23:19 +0300

    title updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,9 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Contact (){
     return(
-        <div>
+        <div lassName="container bg-dark">
             <h1>Contact</h1>
         </div>
     )
