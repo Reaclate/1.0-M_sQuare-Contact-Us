ef6103aa95a969a9c65339ed71b06b015b4d7c44
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-09-12 14:59:26 +0300

    vs code setting changed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Contact (){
     return(
-        <div className="container bg-dark rounded">
-            <h1>Contact Us</h1>
+        <div className="container bg-white mt-4 rounded text-center">
+            <h2 className = "text-decoration-underline text-black" > Contact Us < /h2>
         </div>
+        
     )
 }
