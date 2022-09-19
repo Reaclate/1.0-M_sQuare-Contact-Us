15a3e54ff11b9e949fd271dc28fd3462f2114f5e
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-09-19 23:41:05 +0300

    added a state management

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from '../assets/img/logo-bg.png'
 import illustrator from '../assets/img/illustrator.svg'
 import './contact.css';
 
 export default function Contact (){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    
     return(
         <>
         <div className="container bg-white mt-5 rounded text-center">
@@ -20,13 +24,15 @@ export default function Contact (){
             <form className="container bg-white mt-4 w-100 text-center rounded">
             
             <input type="text" placeholder="Enter your Full Name" 
-            className="w-75 my-4 border rounded border-secondary p-2" />
+            className="w-75 my-4 border rounded border-secondary p-2" onChange={setName} />
 
             <input type="text" placeholder="Enter your Email" 
-            className="w-75 mb-4 border rounded border-secondary p-2" />
+            className="w-75 mb-4 border rounded border-secondary p-2" onChange={setEmail} />
 
             <textarea type="text" placeholder="Message" rows={5} cols={50} 
-            className="textarea w-75 mb-3 border rounded border-secondary p-2 mw-100 mh-25 m ">
+            className="textarea w-75 mb-3 border rounded border-secondary p-2 mw-100 mh-25 m "
+            onChange={setMessage}
+            >
             </textarea>
           
 
