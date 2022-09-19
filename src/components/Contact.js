15bfcc0e060f36ce9ad65285fc9d731717726cac
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-09-19 21:47:36 +0300

    logo update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,13 @@
 import React from "react";
+import logo from '../assets/img/logo-bg.png'
 
 export default function Contact (){
     return(
         <div className="container bg-white mt-4 rounded text-center">
-            <h2 className = "text-decoration-underline text-black" > Contact Us < /h2>
+            <img src={logo} alt="logo" className="img-fluid" />
+            <h2 className ="text-decoration-underline text-black" > 
+            Contact Us 
+            </h2>
         </div>
         
     )
